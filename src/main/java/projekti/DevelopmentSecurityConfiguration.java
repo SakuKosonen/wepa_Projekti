c512c8304fe241627e84948cb92bd73d53e54d69
SakuKosonen <saku.kosonen96@gmail.com>
2020-12-13 16:32:01 +0200

    heii

--- src/main/java/projekti/DevelopmentSecurityConfiguration.java
+++ src/main/java/projekti/DevelopmentSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,9 +20,19 @@ public class DevelopmentSecurityConfiguration extends WebSecurityConfigurerAdapt
     private UserDetailsService userDetailsService;
 
     @Override
-    public void configure(WebSecurity sec) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         // Pyyntöjä ei tarkasteta
-        sec.ignoring().antMatchers("/**");
+        //sec.ignoring().antMatchers("/**");
+        http.csrf().disable();
+        // sallitaan framejen käyttö
+        http.headers().frameOptions().sameOrigin();
+
+        http.authorizeRequests()
+                .antMatchers("/accounts", "/accounts/**").permitAll()                
+                .anyRequest().authenticated().and()
+                .formLogin().permitAll().and()
+                .logout().permitAll();
+
     }
 
     @Autowired
